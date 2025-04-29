b1e2566da4c4cd9720f59e4233165f9256fc12f1
Joao Izzi <joaovictorizzi9@gmail.com>
2025-04-29 11:37:16 -0300

    Codes

---
+++ Codigos/Aplicação_Small/README.md
@@ -0,0 +1,7 @@
+## Detecção de anomalias utilizando o iForest e o Extended iForest para o Small (Statistical Machine Learning Lab).
+
+- O iForest tem implementação no sklearn.ensemble como IsolationForest, enquanto no R em library(isotree).
+
+- O extended iForest tem implementação em python e em R, a referência pode ser encontrada aqui: https://github.com/sahandha/eif
+
+- Não cheguei a testar o eif para o R, apenas verifiquei que no link eles disponibilizam o pacote.
---
+++ Codigos/Aplicação_Small/iforest.py
@@ -0,0 +1,21 @@
+# Aplicação do iforest para o small
+
+# %%
+
+import pandas as pd
+import matplotlib.pyplot
+import seaborn as sns
+
+from sklearn.ensemble import IsolationForest
+from sklearn.model_selection import train_test_split
+
+# %%
+
+df = pd.read_csv("../../Dados/creditcard.csv")
+df
+
+# Explicação variáveis
+# time: Number of seconds elapsed between this transaction and the first transaction in the dataset
+# V1: may be result of a PCA Dimensionality reduction to protect user identities and sensitive features(v1-v28)
+# Amount: Transaction amount
+# Class: 1 for fraudulent transactions, 0 otherwise
